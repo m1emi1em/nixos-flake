2665d3aa48976c13892e80160a34a4d2baa8b80d
Emily W <m1emi1em@proton.me>
2025-01-03 18:59:18 -0500

    squash! squash! mpd, folder testing too

--- stuff/mpd.nix
+++ stuff/mpd.nix
@@ -2,6 +2,7 @@
   services.mpd = {
     enable = true;
     musicDirectory = "/home/emily/Music";
+    dataDir = "$XDG_DATA_HOME/.mpd";
     extraConfig = ''
     audio_output {
                 type "pipewire"
