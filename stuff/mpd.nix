4169bb2fab8bb8888d552dcd4eece51cb4db6460
Emily W <m1emi1em@proton.me>
2025-01-05 10:17:06 -0500

    move }; so it scoped right

--- stuff/mpd.nix
+++ stuff/mpd.nix
@@ -10,18 +10,19 @@
     }
 '';
 
-    services.mpd-discord-rpc = {
-      enable = false;
-
-      settings = {
-        hosts = [ "localhost:6600" ];
-        format = {
-          details = "$title";
-          state = "On $album by $artist";
-        };
-      };
-    };
 
     network.startWhenNeeded = true;
+  };
+
+  services.mpd-discord-rpc = {
+    enable = false;
+
+    settings = {
+      hosts = [ "localhost:6600" ];
+      format = {
+        details = "$title";
+        state = "On $album by $artist";
+      };
+    };
   };
 }
