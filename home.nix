a80ebb5116a1799a37cd2394d346e038205e3882
Emily W <m1emi1em@proton.me>
2025-01-11 01:48:17 -0500

    some indentation

--- home.nix
+++ home.nix
@@ -63,15 +63,15 @@ in
     steam = pkgs.steam.override {
       extraPkgs = pkgs: with pkgs; [
         xorg.libXcursor
-          xorg.libXi
-          xorg.libXinerama
-          xorg.libXScrnSaver
-          libpng
-          libpulseaudio
-          libvorbis
-          stdenv.cc.cc.lib
-          libkrb5
-          keyutils
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
       ];
     };
   };
