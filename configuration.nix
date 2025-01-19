187351950c700fc457a104b1a641fb473bfac3dd
Emily W <m1emi1em@proton.me>
2025-01-19 06:11:06 +0000

    clean up configuration.nix, move docker to virt.nix

--- configuration.nix
+++ configuration.nix
@@ -5,52 +5,6 @@
 { config, lib, pkgs, ... }:
 
 {
-  # imports =
-  #   [ # Include the results of the hardware scan.
-  #     ./hardware-configuration.nix
-  #   ];
-
-  #nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # nix.settings = {
-  #   experimental-features = ["nix-command" "flakes"];
-  #   #substituters = ["https://hyprland.cachix.org"];
-  #   #trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-  # };
-
-
-  # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-
-  #networking.hostName = "Emerald"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  #networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  # time.timeZone = "America/New_York";
-
-  # # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "en_US.UTF-8";
-  #   LC_IDENTIFICATION = "en_US.UTF-8";
-  #   LC_MEASUREMENT = "en_US.UTF-8";
-  #   LC_MONETARY = "en_US.UTF-8";
-  #   LC_NAME = "en_US.UTF-8";
-  #   LC_NUMERIC = "en_US.UTF-8";
-  #   LC_PAPER = "en_US.UTF-8";
-  #   LC_TELEPHONE = "en_US.UTF-8";
-  #   LC_TIME = "en_US.UTF-8";
-  # };
-
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
@@ -67,70 +21,6 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  # hardware.pulseaudio.enable = false;
-  # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   # If you want to use JACK applications, uncomment this
-  #   #jack.enable = true;
-
-  #   # use the example session manager (no others are packaged yet so this is enabled by default,
-  #   # no need to redefine it in your config for now)
-  #   #media-session.enable = true;
-  # };
-
-  #services.pipewire.extraConfig.pipewire."92-low-latency" = {
-  #  "context.properties" = {
-      #"default.clock.rate" = 48000;
-      #"default.clock.quantum" = 2048;
-  #    "default.clock.min-quantum" = 1024;
-      #"default.clock.max-quantum" = 2048;
-  #  };
-  #};
-
-#  services.pipewire.extraConfig.pipewire-pulse."92-low-latency" = {
-#    context.modules = [
-#    {
-#      name = "libpipewire-module-protocol-pulse";
-#      args = {
-#        pulse.min.req = "768/48000";
-#        pulse.default.req = "768/48000";
-#        pulse.max.req = "768/48000";
-#        pulse.min.quantum = "768/48000";
-#        pulse.max.quantum = "768/48000";
-#      };
-#    }
-#    ];
-#    stream.properties = {
-#      node.latency = "768/48000";
-#      resample.quality = 1;
-#    };
-#  };
-
-  # Containers
-  virtualisation = {
-
-    # Docker
-    docker = {
-      enable = true;
-      liveRestore = false;
-
-      daemon.settings = {
-        data-root = "/docker";
-      };
-
-      # rootless = {
-      #  enable = true;
-      # setSocketVariable = true;
-      #};
-
-    };
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
