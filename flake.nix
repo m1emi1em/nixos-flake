5123e83b78f9646fe0b67d3f9a4a87122ad1c41c
Emily W <m1emi1em@proton.me>
2025-01-19 00:38:02 -0500

    cleanup

--- flake.nix
+++ flake.nix
@@ -13,17 +13,15 @@
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-
   };
 
   outputs = { self, nixpkgs, home-manager, nixpkgs-unstable, ... } @ inputs:
   let 
     system = "x86_64-linux";
-    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
-    #pkgs-unstable = import nixpkgs-unstable {inherit system; config.allowUnfree = true; };
+    pkgs-unstable = import nixpkgs-unstable {inherit system; config.allowUnfree = true; };
     pkgs = nixpkgs.legacyPackages.${system};
 
+    # idk how to use this :3
     forAllSystems =
       function:
       nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
@@ -36,20 +34,12 @@
       );
   in
   {
-
-    pkgs.config.allowUnfree = true;
-    
     # Please replace my-nixos with your hostname
     nixosConfigurations.Emerald = nixpkgs.lib.nixosSystem {
-      #system = "x86_64-linux";
-      #inherit system;
       specialArgs = {
         inherit inputs;
-        #system = "x86_64-linux";
       };
 
-      
-      
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
@@ -74,18 +64,11 @@
               
             };
 
-
             # Personal account
             home-manager.users.emily = import ./home.nix;
           }
         
-
         ({nixpkgs, inputs, home-manager, ...}: {
-          # home-manager.nixosModules.home-manager = {
-          #   extraSpecialArgs = {
-          #     inherit inputs;
-          #   };
-          # };
           imports = [
             ./home/testificate
           ];
