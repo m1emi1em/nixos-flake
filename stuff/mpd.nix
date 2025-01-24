1ce96cd63e7679577a3efed8684c6385b24c464e
m1emi1em <m1emi1em@proton.me>
2025-01-24 13:23:18 -0500

    add mpd-mpris, mdpris

--- stuff/mpd.nix
+++ stuff/mpd.nix
@@ -1,30 +1,42 @@
 {config, pkgs-unstable, ...}:
 {
-  services.mpd = {
-    enable = true;
-    musicDirectory = "${config.xdg.userDirs.music}";
-    extraConfig = ''
+  services = {
+    mpd = {
+      enable = true;
+      musicDirectory = "${config.xdg.userDirs.music}";
+      extraConfig = ''
     audio_output {
                 type "pipewire"
                 name "MPD (Music Player Daemon)"
     }
 '';
+      network.startWhenNeeded = true;
+    };
 
-
-    network.startWhenNeeded = true;
-  };
-
-  services.mpd-discord-rpc = {
-    enable = true;
-
-    settings = {
-      hosts = [ "localhost:6600" ];
-      format = {
-        details = "$title";
-        state = "On $album by $artist";
+    mpd-discord-rpc = {
+      enable = true;
+      settings = {
+        hosts = [ "localhost:6600" ];
+        format = {
+          details = "$title";
+          state = "On $album by $artist";
+        };
       };
+    };
+
+    mpd-mpris = {
+      enable = true;
+      mpd = {
+        useLocal = true;
+      };
+    };
+
+    mpdris2 = {
+      enable = true;
+      notifications = true;
     };
   };
 
   home.packages = [ pkgs-unstable.rmpc ];
 }
+  
