c278066644350d5367702e02c1b7927d3b10f246
m1emi1em <m1emi1em@proton.me>
2025-03-08 21:32:09 -0500

    chore: move hyprland to separate file

--- stuff/hyprland.nix
+++ stuff/hyprland.nix
@@ -1,4 +1,25 @@
 {pkgs, ...}:
+let
+  hyprland-packages = with pkgs; [
+    
+    hyprpolkitagent # Auth Agent # gui
+    hyprpaper
+    dunst # Notification Daemon # gui
+    waybar # Status bar # gui
+    waypaper  # gui
+
+    xdg-desktop-portal-hyprland # gui
+    wofi # gui
+    hyprpicker # gui
+
+    hypridle # gui
+    hyprlock # gui
+    #hyprsysteminfo
+    hyprsunset # gui
+    
+  ];
+in
 {
-  home.packages = with pkgs; [qalculate-qt];
+  home.packages = hyprland-packages;
+  
 }
