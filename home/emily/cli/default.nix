d6a0da2567b3e72df13e051f68bc9d8fcd6e5bd9
m1emi1em <m1emi1em@proton.me>
2025-05-10 11:00:16 -0400

    feat: add tealdeer w/ auto-update interval of 24 hours

--- home/emily/cli/default.nix
+++ home/emily/cli/default.nix
@@ -22,6 +22,7 @@ in
     ./nvim.nix
     ./pfetch.nix
     ./yazi.nix
+    ./tealdeer.nix
   ]; 
 
   home.packages = cliPackages;
---
+++ home/emily/cli/tealdeer.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  programs.tealdeer = {
+    enable = true;
+    settings = {
+      display = {
+        compact = true;
+      };
+      updates = {
+        auto_update = true;
+        auto_update_interval_hours = 24;
+      };
+    };
+  };
+}
