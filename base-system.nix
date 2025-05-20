5e55a9cdf234639ac396542c11092f7bfd362ee9
m1emi1em <m1emi1em@proton.me>
2025-05-20 12:35:19 -0400

    feat(base-system): change default dm session to hyprland-uwsm

--- base-system.nix
+++ base-system.nix
@@ -9,7 +9,11 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
+  services.displayManager = {
+    sddm.enable = true;
+    defaultSession = "hyprland-uwsm";
+  };
+  
   services.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
