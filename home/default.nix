fd4ded5d66b1b9fe10fed2abcbd74d5eb538d634
m1emi1em <m1emi1em@proton.me>
2025-05-25 10:28:17 -0400

    feat(home): add backupFileExtension to home-manager
    bc manually shuffling stuff sometimes is annoying ngl

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,7 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     users.emily = ./emily; #./../home.nix;
+    backupFileExtension = "backup";
   };
 
   nixpkgs.overlays = [inputs.hyprpanel.overlay];
