a5bd30092090a3041fb1f986cb158e5b28cc9764
m1emi1em <m1emi1em@proton.me>
2025-05-27 10:27:28 -0400

    feat(walker): add default config and app_launch_prefix

--- home/emily/gui/hyprland/walker.nix
+++ home/emily/gui/hyprland/walker.nix
@@ -1,9 +1,13 @@
-{ pkgs, inputs, ...} : {
+{ lib, pkgs, inputs, ...} :
+let
+  inherit (lib.trivial) importTOML;
+in {
   imports = [ inputs.walker.homeManagerModules.walker ];
 
   programs.walker = {
     enable = true;
     runAsService = true;
     package = pkgs.walker;
+    config = importTOML ./walker.toml;
   };
 }
