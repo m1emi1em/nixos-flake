ce917a164094959bdf084edf17de2326ffcd770d
m1emi1em <m1emi1em@proton.me>
2025-07-15 19:46:37 -0400

    feat: add nix-community cachix

--- base-system.nix
+++ base-system.nix
@@ -113,8 +113,10 @@
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "emily"];
       substituters = [
+        "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
     gc = {
