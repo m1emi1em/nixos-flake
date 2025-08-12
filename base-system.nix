572590665707f8ba1b1f8ff57d1762f09cf37fd7
m1emi1em <m1emi1em@proton.me>
2025-08-12 13:25:43 -0400

    fix(syncthing): just remove the gui creds since we're not opening it up anyway

--- base-system.nix
+++ base-system.nix
@@ -199,11 +199,6 @@
       enable = true;
       openDefaultPorts = true;
       settings = {
-        gui = {
-          user = "emily";
-          password = "don'tfuuckingcommitthispasswordyoubsolutefool";
-        };
-
         devices = {
           "Bismuth" = { id = "HVYM6NR-CSG4XFD-6V76ABE-TFFXKSC-Q4NWO3R-ZPGAUBP-OSMVOGD-GCASDAT"; };
         };
