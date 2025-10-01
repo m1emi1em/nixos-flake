752a3f900593ad5fd4e65f7571cb8c7e0191ecb6
m1emi1em <m1emi1em@proton.me>
2025-10-01 14:53:07 -0300

    feat(yazi): more directory shortcuts + nix helpers for adding more

--- home/emily/cli/yazi/default.nix
+++ home/emily/cli/yazi/default.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 let
   inherit (lib.trivial) importTOML;
+  inherit (lib) stringToCharacters;
+  inherit (builtins) elemAt;
 in
 {
   programs.yazi = {
@@ -13,12 +15,28 @@ in
 '';
     settings = importTOML ./yazi.toml;
 
-    keymap = {
-      mgr.prepend_keymap = [
-        {
-          run = "cd ${config.xdg.userDirs.pictures}";
-          on = [ "g" "p" ]; }
-      ];
+    keymap =  let
+      genKeymapEntry = runCommand: keymapString:
+          {
+            run = "${runCommand}";
+            on = stringToCharacters keymapString;
+          };
+      directoryJump = targetDirectory: shortcut:
+        genKeymapEntry "cd ${targetDirectory}" shortcut;
+    in {
+      mgr.prepend_keymap = let
+        makeJumps = list:
+          map (a: directoryJump (elemAt a 0) (elemAt a 1)) list;
+      in [
+        # (directoryJump config.xdg.userDirs.pictures "gp")
+        # (directoryJump config.xdg.userDirs.videos "gv")
+      ] ++ makeJumps (with config.xdg; [
+        [userDirs.pictures "gp"]
+        [userDirs.videos "gv"]
+        [userDirs.music "gm"]
+        [userDirs.documents "gD"]
+        ["/home/emily/dev" "gw"]
+      ]);
     };
   };
   home.shellAliases = {
