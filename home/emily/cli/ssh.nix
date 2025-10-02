bf11fcfa10203ae37df86d857b6f0b05f4d9e657
m1emi1em <m1emi1em@proton.me>
2025-10-02 09:36:48 -0300

    feat(ssh): define custom hosts here in flake rather than importing external config

--- home/emily/cli/ssh.nix
+++ home/emily/cli/ssh.nix
@@ -1,18 +1,29 @@
-{...}:
+{ ... }:
 {
   programs.ssh = {
     enable = true;
-    includes = [ "~/.config/ssh/config" ];
-    #matchBlocks = {
-    #  "m1-gitea" = {
-    #    hostname = "localhost";
-    #    user = "git";
-    #    port = "32769";
-    #    identitiesOnly = true;
-    #    identityFile = "~/.ssh/id_m1_gh";
-
-    #  };
-    #};
     enableDefaultConfig = false;
+    # includes = [ "~/.config/ssh/config" ];
+    matchBlocks = {
+      "m1-gitea" = {
+        hostname = "localhost";
+        user = "git";
+        identityFile = "~/.ssh/id_gh";
+        identitiesOnly = true;
+      };
+      "m1-github" = {
+        hostname = "github.com";
+        user = "git";
+        port = "32769";
+        identityFile = "~/.ssh/id_gh";
+        identitiesOnly = true;
+      };
+      "m1-tangled" = {
+        hostname = "tangled.org";
+        user = "git";
+        identityFile = "~/.ssh/id_tngld";
+        identitiesOnly = true;
+      };
+    };
   };
 }
