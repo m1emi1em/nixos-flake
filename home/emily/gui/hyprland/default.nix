9907544e7a08a20c7214b19e78c9c991b7e0d3f0
m1emi1em <m1emi1em@proton.me>
2025-10-02 09:02:23 -0300

    feat(hyprland): reduce all gap sizes + border size

--- home/emily/gui/hyprland/default.nix
+++ home/emily/gui/hyprland/default.nix
@@ -62,9 +62,9 @@ in
           ];
 
           general = {
-            "gaps_in" = 3;
-            "gaps_out" = 5;
-            "border_size" = 2;
+            "gaps_in" = 2;
+            "gaps_out" = 1;
+            "border_size" = 1;
             "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
             "col.inactive_border" = "rgba(595959aa)";
             "resize_on_border" = false;
